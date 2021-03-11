4dceb371e8fbc614a5e875c0a0af8372a067cb46
Luvo <luislivilla@gmail.com>
2021-03-11 15:31:47 +0100

    Fix regex escape backlashes (#11)
    * Fix regex escape backlashes
    
    * Removes comment as we escape \ everywhere

--- cmake/modules/FindRTIConnextDDS.cmake
+++ cmake/modules/FindRTIConnextDDS.cmake
@@ -1058,10 +1058,7 @@ string(REGEX MATCH
     "${build_id_line}"
 )
 string(REGEX MATCH
-    # Double scape was added for the last digit because the CMake regex
-    # engine gets the following characters. Doing this, everything works
-    # properly
-    "[0-9]+\.[0-9]+\.[0-9]+(\\.[0-9]+)?"
+   "[0-9]+\\.[0-9]+\\.[0-9]+(\\.[0-9]+)?"
     RTICONNEXTDDS_VERSION
     "${CONNEXTDDS_BUILD_ID}"
 )
